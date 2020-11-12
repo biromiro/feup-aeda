a098fead12887260383340a1516082ac97ac9445
Nuno Costa <n.r.t.c@hotmail.com>
2020-11-12 17:23:29 +0000

    1819 ci2

---
+++ aeda1819_ci2/Tests/CStack.h
@@ -0,0 +1,60 @@
+// CStack.h - uma Stack simples de inteiros implementada com arrays
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CStack { 
+    int top, d_size, *items;
+public:
+    CStack(int size) { top = -1; d_size = size; items = new int[size]; }
+    ~CStack() { delete []items;} 
+    bool empty() const { return top==-1; } 
+    int size() const { return top+1; } 
+    void push(int elem) { 
+        if(size()==d_size) cerr << "Stack Full!\n";
+        else { top++; items[top] = elem; }
+    }
+    void pop() { 
+        if(this->empty()) cerr << "Empty stack\n";
+        else top--;
+    }
+    int peek() const { 
+        if(this->empty()) cerr << "Empty stack\n";
+        else return items[top];
+        return 0;
+    }
+    string toStr() const {
+        stringstream oss;
+        for(int i = size()-1; i>=0; i--)
+        	oss << items[i] << " ";
+        return oss.str();
+    }
+    void print() const { 
+        cout << "Stack with " << size() << " elements: ";
+        for(int i = size()-1; i>=0; i--)
+            cout << items[i] << " ";
+        cout << endl;
+    }
+    void adicionaN(int n)
+	{
+    	for(;n>0;n--){
+    	    push(peek()+1);
+    	}
+	}
+	bool inverte4() 
+	{
+        if(size() < 4)
+            return false;
+        vector<int> toSort;
+        for(size_t i=0; i<4; ++i){
+            toSort.push_back(peek());
+            pop();
+        }
+        for(auto const& elem: toSort)
+            push(elem);
+    	return true;
+	}	
+}; 
+
